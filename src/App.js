e87b310ee38f0e8ca2097173a1283de2c2f7c85d
Sean Kozen <sgkozen@gmail.com>
2023-02-10 14:15:48 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+
+createBrowserRouter([
+  { path: '/',  },
+  { path: '' },
+]);
+
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
